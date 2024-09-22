624293e795ef7da4ebab19403d137d603c647210
Caio Lopes <caio.lopes2@fatec.sp.gov.br>
2024-09-22 20:20:14 -0300

    p1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VitrineComponent } from './vitrine/vitrine.component';
 
 @Component({
   selector: 'app-root',
